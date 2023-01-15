60608a09492cbbbc5c05bae363115491adb2f594
k12.tamaki <k12.tamaki@gmail.com>
2023-01-15 10:09:43 +0900

    2022/01/15 ベースの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './media/logo.svg';
+// import './css/App.css';
+import Top from './Top'
+
+// import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+// import Button from "@mui/material/Button";
+import Header from "./components/header";
+// import { Box, Stack } from "@mui/material";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header />
+      {/* <Box
+        sx={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          p: 1,
+          m: 1,
+          bgcolor: "background.paper",
+          borderRadius: 1,
+        }}
+      >
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained" color="primary">
+            メインカラー(contained)
+          </Button>
+          <Button variant="outlined" color="secondary">
+            セカンダリーカラー(outlined)
+          </Button>
+          <Button variant="text" color="success">
+            サクセスカラー(text)
+          </Button>
+        </Stack>
+      </Box> */}
+      <Top />
+    </ThemeProvider>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Top.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+function Top() {
+  return (
+    <div className="App" style={{height: '2000px'}}>
+      Top
+    </div>
+  );
+}
+
+export default Top;
