d42be3536d22753c0db02ef144466dc991739077
katsunari <k12.tamaki@gmail.com>
2023-01-31 23:06:03 +0900

    2023/01/31 キャリア雑修正

--- src/components/CareerCard.tsx
+++ src/components/CareerCard.tsx
@@ -7,55 +7,54 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+// import Button from '@mui/material/Button';
+// import { styled } from '@mui/material/styles';
+// import Dialog from '@mui/material/Dialog';
+// import DialogTitle from '@mui/material/DialogTitle';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogActions from '@mui/material/DialogActions';
+// import IconButton from '@mui/material/IconButton';
+// import CloseIcon from '@mui/icons-material/Close';
 // import Typography from '@mui/material/Typography';
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
-  },
-}));
+// const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+//   '& .MuiDialogContent-root': {
+//     padding: theme.spacing(2),
+//   },
+//   '& .MuiDialogActions-root': {
+//     padding: theme.spacing(1),
+//   },
+// }));
 
 /* eslint-disable */
-export interface DialogTitleProps {
-  id: string;
-  children?: React.ReactNode;
-  onClose: () => void;
-}
 
-function BootstrapDialogTitle(props: DialogTitleProps) {
-  const { children, onClose, ...other } = props;
+// const BootstrapDialogTitle: React.FC<{
+//   id: string;
+//   children?: React.ReactNode;
+//   onClose: () => void;
+// }> = ({ id, children, onClose }) => {
+//   // const { children, onClose, ...other } = props;
 
-  return (
-    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-      {children}
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </DialogTitle>
-  );
-}
+//   return (
+//     <DialogTitle sx={{ m: 0, p: 2 }}>
+//       {children}
+//       {onClose ? (
+//         <IconButton
+//           aria-label="close"
+//           onClick={onClose}
+//           sx={{
+//             position: 'absolute',
+//             right: 8,
+//             top: 8,
+//             color: (theme) => theme.palette.grey[500],
+//           }}
+//         >
+//           <CloseIcon />
+//         </IconButton>
+//       ) : null}
+//     </DialogTitle>
+//   );
+// };
 
 const CareerCard: React.FC<{
   imagePath: any;
@@ -88,7 +87,7 @@ const CareerCard: React.FC<{
         </CardActionArea>
       </Card>
 
-      <BootstrapDialog
+      {/* <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
@@ -121,7 +120,7 @@ const CareerCard: React.FC<{
             Save changes
           </Button>
         </DialogActions>
-      </BootstrapDialog>
+      </BootstrapDialog> */}
     </Box>
   );
 };
