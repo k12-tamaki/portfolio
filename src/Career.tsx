a128a1c28d65c33de83149abbe4e3d3a062b9586
katsunari <k12.tamaki@gmail.com>
2023-02-02 22:33:03 +0900

    2022/02/02 キャリアの修正

--- src/Career.tsx
+++ src/Career.tsx
@@ -8,6 +8,37 @@ import CareerCard from './components/CareerCard';
 import './css/common.css';
 
 function Career() {
+  interface cardObj {
+    imagePath: string;
+    cardTitle: string;
+  }
+
+  const workArrayObject: cardObj[] = [
+    {
+      imagePath: '/static/website_normal.png',
+      cardTitle: 'Webシステム',
+    },
+    {
+      imagePath: '/static/website_normal.png',
+      cardTitle: 'IoTシステム',
+    },
+    {
+      imagePath: '/static/website_normal.png',
+      cardTitle: 'ロボット',
+    },
+  ];
+
+  const hobbyArrayObject = [
+    {
+      imagePath: '/static/website_normal.png',
+      cardTitle: 'ポートフォリオ',
+    },
+    {
+      imagePath: '/static/website_normal.png',
+      cardTitle: '結婚式HP',
+    },
+  ];
+
   return (
     <Box className="mainContent">
       <Box className="title">Career</Box>
@@ -15,40 +46,25 @@ function Career() {
         <Box className="profile">
           <Box sx={{ fontSize: '200%' }}>Work</Box>
           <Grid container spacing={2}>
-            <Grid xs={4}>
-              <CareerCard
-                imagePath="/static/website_normal.png"
-                cardTitle="Webシステム"
-              />
-            </Grid>
-            <Grid xs={4}>
-              <CareerCard
-                imagePath="/static/website_normal.png"
-                cardTitle="IoTシステム"
-              />
-            </Grid>
-            <Grid xs={4}>
-              <CareerCard
-                imagePath="/static/website_normal.png"
-                cardTitle="ロボット"
-              />
-            </Grid>
+            {workArrayObject.map((workObject, workIndex) => (
+              <Grid xs={4} key={workIndex}>
+                <CareerCard
+                  imagePath={workObject.imagePath}
+                  cardTitle={workObject.cardTitle}
+                />
+              </Grid>
+            ))}
           </Grid>
           <Box sx={{ fontSize: '200%', marginTop: '50px' }}>Hobby</Box>
           <Grid container spacing={2}>
-            <Grid xs={4}>
-              <CareerCard
-                imagePath="/static/website_normal.png"
-                cardTitle="ポートフォリオ"
-              />
-            </Grid>
-            <Grid xs={4}>
-              <CareerCard
-                imagePath="/static/website_normal.png"
-                cardTitle="結婚式HP"
-              />
-            </Grid>
-            <Grid xs={4}></Grid>
+            {hobbyArrayObject.map((hobbyObject, hobbyIndex) => (
+              <Grid xs={4} key={hobbyIndex}>
+                <CareerCard
+                  imagePath={hobbyObject.imagePath}
+                  cardTitle={hobbyObject.cardTitle}
+                />
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </Box>
--- src/components/CareerCard.tsx
+++ src/components/CareerCard.tsx
@@ -8,57 +8,52 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
 // import Button from '@mui/material/Button';
-// import { styled } from '@mui/material/styles';
-// import Dialog from '@mui/material/Dialog';
-// import DialogTitle from '@mui/material/DialogTitle';
-// import DialogContent from '@mui/material/DialogContent';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
 // import DialogActions from '@mui/material/DialogActions';
-// import IconButton from '@mui/material/IconButton';
-// import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 // import Typography from '@mui/material/Typography';
 
-// const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-//   '& .MuiDialogContent-root': {
-//     padding: theme.spacing(2),
-//   },
-//   '& .MuiDialogActions-root': {
-//     padding: theme.spacing(1),
-//   },
-// }));
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
-/* eslint-disable */
-
-// const BootstrapDialogTitle: React.FC<{
-//   id: string;
-//   children?: React.ReactNode;
-//   onClose: () => void;
-// }> = ({ id, children, onClose }) => {
-//   // const { children, onClose, ...other } = props;
-
-//   return (
-//     <DialogTitle sx={{ m: 0, p: 2 }}>
-//       {children}
-//       {onClose ? (
-//         <IconButton
-//           aria-label="close"
-//           onClick={onClose}
-//           sx={{
-//             position: 'absolute',
-//             right: 8,
-//             top: 8,
-//             color: (theme) => theme.palette.grey[500],
-//           }}
-//         >
-//           <CloseIcon />
-//         </IconButton>
-//       ) : null}
-//     </DialogTitle>
-//   );
-// };
+const BootstrapDialogTitle: React.FC<{
+  children?: React.ReactNode;
+  onClose: () => void;
+}> = ({ children, onClose }) => {
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
 
 const CareerCard: React.FC<{
-  imagePath: any;
-  cardTitle: any;
+  imagePath: string;
+  cardTitle: string;
 }> = ({ imagePath, cardTitle }) => {
   const [open, setOpen] = React.useState(false);
 
@@ -68,6 +63,8 @@ const CareerCard: React.FC<{
   const handleClose = () => {
     setOpen(false);
   };
+
+  const isWebSystem = cardTitle == 'Webシステム';
 
   return (
     <Box>
@@ -87,40 +84,41 @@ const CareerCard: React.FC<{
         </CardActionArea>
       </Card>
 
-      {/* <BootstrapDialog
+      <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <BootstrapDialogTitle
-          id="customized-dialog-title"
-          onClose={handleClose}
-        >
-          Modal title
+        <BootstrapDialogTitle onClose={handleClose}>
+          {cardTitle}
         </BootstrapDialogTitle>
         <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
+          {isWebSystem && (
+            <Box>
+              <Typography gutterBottom>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo
+                odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
+                risus, porta ac consectetur ac, vestibulum at eros.
+              </Typography>
+              <Typography gutterBottom>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur
+                et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus
+                dolor auctor.
+              </Typography>
+              <Typography gutterBottom>
+                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
+                cursus magna, vel scelerisque nisl consectetur et. Donec sed
+                odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+              </Typography>
+            </Box>
+          )}
         </DialogContent>
-        <DialogActions>
+        {/* <DialogActions>
           <Button autoFocus onClick={handleClose}>
             Save changes
           </Button>
-        </DialogActions>
-      </BootstrapDialog> */}
+        </DialogActions> */}
+      </BootstrapDialog>
     </Box>
   );
 };
